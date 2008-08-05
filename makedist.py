11dfba26bf417b070abe982da940c6cc283a12fa
Bruce Guenter <bruce@untroubled.org>
2008-08-05 17:49:23 +0000

    Added makedist.py file.

---
+++ makedist.py
@@ -0,0 +1,15 @@
+WEBSITE = 'untroubled.org'
+LISTSUB = 'bgware-subscribe@lists.untroubled.org'
+LISTURL = 'http://lists.untroubled.org/?list=bgware'
+EXTRAS = [
+	'ANNOUNCEMENT',
+	'ChangeLog',
+	'NEWS',
+	'README',
+	'TODO',
+	#'doc/html',
+	]
+RPMUPLOAD = [ ]
+LISTS = [
+	'bgware@lists.untroubled.org',
+	]
